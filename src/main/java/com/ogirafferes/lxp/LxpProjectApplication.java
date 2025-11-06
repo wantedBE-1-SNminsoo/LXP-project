35f7259faadb762260d0ac5e1c45283c67fdc7b9
EomKun <zzang6535@gmail.com>
2025-11-06 11:01:28 +0900

    first commit

---
+++ src/main/java/com/ogirafferes/lxp/LxpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ogirafferes.lxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LxpProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogirafferes/lxp/LxpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogirafferes.lxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
