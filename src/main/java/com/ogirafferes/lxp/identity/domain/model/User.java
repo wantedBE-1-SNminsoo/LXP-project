7c1445da2bb2ccd852104a1dc0b4f96e93269f02
minji0828 <entersuk1998@naver.com>
2025-11-10 15:31:10 +0900

    fix(user)
    user 테이블 명 수정

--- src/main/java/com/ogirafferes/lxp/identity/domain/model/User.java
+++ src/main/java/com/ogirafferes/lxp/identity/domain/model/User.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users1")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
