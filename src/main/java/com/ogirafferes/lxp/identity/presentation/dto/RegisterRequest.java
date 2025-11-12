d6b39075060a812be9ce96799c827ea6ae91b315
minji0828 <entersuk1998@naver.com>
2025-11-12 14:22:43 +0900

    fix(identity) DTO setter 추가
    setter 추가 및 기타 오타 수정
    미사용 userDetailsAdapter 삭제
    (customuserprincipal이 대체)

--- src/main/java/com/ogirafferes/lxp/identity/presentation/dto/RegisterRequest.java
+++ src/main/java/com/ogirafferes/lxp/identity/presentation/dto/RegisterRequest.java
@@ -1,6 +1,5 @@
 package com.ogirafferes.lxp.identity.presentation.dto;
 
-import com.ogirafferes.lxp.identity.domain.model.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -9,10 +8,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class RegisterRequest {
-
-
 
 	@NotBlank(message = "사용자명은 필수입니다.")
 	@Size(min = 3, max = 20, message = "사용자명은 3자 이상 20자 이하여야 합니다.")
@@ -26,8 +24,7 @@ public class RegisterRequest {
 	@Size(max = 100, message = "닉네임은 100자 이하여야 합니다.")
 	private String nickname;
 
-    @NotNull(message = "역할은 필수")
-    private Long roleId;
-
+	@NotNull(message = "역할은 필수입니다.")
+	private Long roleId;
 }
 
