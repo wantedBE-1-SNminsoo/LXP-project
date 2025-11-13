d9ab6d8b766d0180ac9e531ab862675299b5a8f6
minji0828 <entersuk1998@naver.com>
2025-11-13 11:55:04 +0900

    feat(template) html 수정

--- src/main/java/com/ogirafferes/lxp/learning/presentation/http/LearningController.java
+++ src/main/java/com/ogirafferes/lxp/learning/presentation/http/LearningController.java
@@ -1,9 +1,11 @@
 package com.ogirafferes.lxp.learning.presentation.http;
 
+import com.ogirafferes.lxp.identity.application.adapter.CustomUserPrincipal;
 import com.ogirafferes.lxp.learning.application.LearningService;
 import com.ogirafferes.lxp.learning.domain.model.Enrollment;
 import com.ogirafferes.lxp.learning.presentation.dto.EnrollmentWithProgressResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.Currency;
 
 @Controller
 @RequestMapping("/enrollment")
@@ -21,8 +24,8 @@ public class LearningController {
     private final LearningService learningService;
 
     @PostMapping("/{enrollmentId}/completeLecture/{lectureId}") // 해당 요청의 자연스러운 위치는 lecture 에서 complete button을 통해 redirect?
-    public String completeLecture(@PathVariable Long enrollmentId, @PathVariable Long lectureId, Principal principal, Model model) {
-        Long userId = Long.parseLong(principal.getName());
+    public String completeLecture(@PathVariable Long enrollmentId, @PathVariable Long lectureId, @AuthenticationPrincipal CustomUserPrincipal principal, Model model) {
+        long userId = principal.getUserId();
         try {
             Long courseId = learningService.completeLecture(enrollmentId, lectureId, userId);
             return "redirect:/courses/" + courseId;
